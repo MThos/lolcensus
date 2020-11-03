d1ec22d51c11be7370a1abbb48f90d750e1a2b1a
Mykel Agathos <MThos@users.noreply.github.com>
2020-11-03 09:17:51 -0500

    name, title and lore working for individual champs

--- lolcensus/champion/urls.py
+++ lolcensus/champion/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
+from . import views
 
 app_name = 'champion'
 urlpatterns = [
-    path('', ChampionList.as_view(), name='champion'),
+    path('', views.champion_list, name='champion_list'),
+    path('<champion_name>', views.champion, name='champion')
 ]
--- lolcensus/champion/views.py
+++ lolcensus/champion/views.py
@@ -2,26 +2,43 @@ import datetime
 import json
 import os
 from cassiopeia import Champion, Champions
-from django.views import View
 from django.utils import translation
 from django.shortcuts import render
 from django.conf import settings
 
 
-class ChampionList(View):
-    def get(self, request):
-        year = datetime.datetime.now().year
-        get_current_lang = translation.get_language()
-        data = open(os.path.join(settings.BASE_DIR,  'champion/static/champion/ddragon/dragontail-10.2.1/10.2.1/data/en_us/champion.json')).read()
-        champion_data = json.loads(data)
-        champion_dict = {}
-        for key in champion_data['data']:
-            row = champion_data['data'][key]
-            champion_dict[row['name']] = row['image']['full']
+def champion_list(request):
+    year = datetime.datetime.now().year
+    get_current_lang = translation.get_language()
+    data = open(os.path.join(settings.BASE_DIR,  'champion/static/champion/ddragon/dragontail-10.2.1/10.2.1/data/en_us/champion.json')).read()
+    champion_data = json.loads(data)
+    champion_dict = {}
+    for key in champion_data['data']:
+        row = champion_data['data'][key]
+        champion_dict[row['name']] = row['image']['full']
 
-        return render(request, "champion/index.html", {
-            "year": year,
-            "get_current_language": get_current_lang,
-            "tab_title": "League Census",
-            "champion_dict": champion_dict,
-        })
+    return render(request, "champion/index.html", {
+        "year": year,
+        "get_current_language": get_current_lang,
+        "tab_title": "League Census",
+        "champion_dict": champion_dict,
+    })
+
+
+def champion(request, champion_name):
+    year = datetime.datetime.now().year
+    get_current_lang = translation.get_language()
+    data = open(os.path.join(settings.BASE_DIR,  'champion/static/champion/ddragon/dragontail-10.2.1/10.2.1/data/en_us/champion/' + champion_name + '.json')).read()
+    json_dump = json.loads(data)
+    champion_data = {}
+    for key, value in json_dump['data'][champion_name].items():
+        champion_data[key] = [value][0]
+
+    print(champion_data)
+
+    return render(request, "champion/champion.html", {
+        "year": year,
+        "get_current_language": get_current_lang,
+        "tab_title": "League Census",
+        "champion_data": champion_data,
+    })
