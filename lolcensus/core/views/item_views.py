827b0fa121ad9291791f777821528a1520f1f3c4
Mykel Agathos <MThos@users.noreply.github.com>
2021-01-13 07:29:52 -0500

    maps allowed for item working

--- lolcensus/core/views/item_views.py
+++ lolcensus/core/views/item_views.py
@@ -34,17 +34,19 @@ def item(request, item_id):
     for key, value in item_json_dump['data'][item_id].items():
         item_data[key] = [value][0]
 
-    map_data = {}
-    for key in item_json_dump['data'][item_id]['maps'].items():
-        get_map_details(value)
+    map_data = {'mapName': []}
+    for key, value in item_json_dump['data'][item_id]['maps'].items():
+        if value:
+            map_data['mapName'].append(get_map_details(key))
 
-    # print(item_data)
+    print(map_data)
 
     return render(request, "item/item.html", {
         "year": get_year(),
         "get_current_language": get_language(),
         "tab_title": "League Census",
         "item_data": item_data,
+        "map_data": map_data,
         "item_id": item_id,
         "patch": get_patch(),
     })
@@ -54,7 +56,9 @@ def get_map_details(map_id):
     data = open(os.path.join(settings.BASE_DIR, 'core/static/core/json/maps.json')).read()
     map_json_dump = json.loads(data)
 
-    return 0
+    for maps in map_json_dump:
+        if maps["mapId"] == int(map_id):
+            return maps["mapName"]
 
 
 def get_year():
